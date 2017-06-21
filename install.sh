a7837ed62572fd65eb215cb614ba26693eeb4429
Alistair Y. Lewars <alistair.lewars@gmail.com>
2017-06-21 14:14:14 -0400

    Update to working install script.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 function pout(){
     local out=$1
@@ -7,32 +7,34 @@ function pout(){
 
 function save(){
     local fl=$1
-
-    pout "The following files will be saved"
-    [ -f $HOME/$1 ] && mv -v $HOME/${1}{,.save}
+    if [[ -f $HOME/$1 && ! -h $HOME/$1 ]];then mv -v $HOME/${1}{,.save}; fi
 }
 function mklnk(){
     local fl=$1
 
     dotdir=$(pwd)
-    pushd $HOME
-    pout "The following files will be installed"
-    ln -vs ${dotdir}/$1 $1
-    popd
+    pushd $HOME >/dev/null
+    ln -fvs ${dotdir/${HOME}/.}/$1 $1
+    popd >/devn/null
 }
 
 # copy over bash profiles and rc
+pout "The following bash profile files will be saved:"
 save .bash_profile
-mklnk .bash_profile
 save .bashrc
+pout "The following bash profile file will be installed:"
+mklnk .bash_profile
 mklnk .bashrc
 
 # copy over .emacs
+pout "The following emacs config file will be saved:"
 save .emacs
-mklink .emacs
+pout "The following emacs file will be installed:"
+mklnk .emacs
 
 # copy over git files
+pout "The following git files will be saved and installed:"
 for g in git/.git*;do
     save $g
-    mklnk $g
+    mklnk $(basename $g)
 done

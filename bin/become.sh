a7acfce1ed6b92182162bb3ae2ffeafc1de10eae
Alistair Y. Lewars <alistair.lewars@gmail.com>
2023-06-07 21:16:38 -0400

    Update

--- bin/become.sh
+++ bin/become.sh
@@ -3,7 +3,7 @@
 # This script is used to pass the credentials to authenticate to ansible using the current session and op
 
 # Sign into 1password
-eval $(op signin --account MIED6OC6IFGVBDDFOWINOGBFCE)
+eval $(op signin)
 
 # Print secret to standout using secret reference
 op read op://Personal/ansible-become-password/password
